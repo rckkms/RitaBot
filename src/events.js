af1a18d802b1e3e85bd8bc0b7ac21aa9bcc11400
rckkms <rckkms@gmail.com>
2022-03-24 23:37:42 -0600

    Update events.js

--- src/events.js
+++ src/events.js
@@ -76,7 +76,7 @@ exports.listen = function listen (client)
 
          client.user.setPresence({
             "activity": {
-               "name": "ritabot.gg | !tr help",
+               "name": "ScriptureAlone translation",
                "type": "PLAYING"
             },
             "status": "online"
